7470c15b4f6a61c1b40067c15aea7b1ff407f3a0
Kin Lane <info@apievangelist.com>
2015-07-24 23:23:32 -0700

    tweak url

--- js/navigator-apis-json.js
+++ js/navigator-apis-json.js
@@ -72,7 +72,7 @@ function APIJSONNavigatorGetHeaderCell(name,description,url,image,apijsonurl)
     html = html + '<a href="' + url + '" title="' + name + '"><img src="' + image + '" width="175" align="left" style="padding: 15px;" /></a>';
 
     html = html + '<a href="' + url + '" style="color: #000; font-size: 22px; text-decoration: none;" title="' + name + '"><strong>' + name + '</strong></a>';
-   html = html + '  (<a href="' + url + '" style="color: #000; font-size: 18px;" title="' + name + '">apis.json</a>)';
+   html = html + '  (<a href="/apis.json" style="color: #000; font-size: 18px;" title="' + name + '">apis.json</a>)';
     html = html + '<br />' + description;
 
 	return html;
